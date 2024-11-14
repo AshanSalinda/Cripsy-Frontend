5c97e0f4c27db507af7b674187bd1e60f4259fdd
AshanSalinda <ashansalinda5@gmail.com>
2024-11-14 17:37:58 +0530

    Product Items Details Updated

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -23,20 +23,25 @@ const ProductItem : React.FC = () => {
     return (
         <div>
             {/* Product Item Details */}
-            <div className="flex items-center bg-black h-[92vh]">
-                <div className="flex flex-wrap justify-between items-center w-full">
+            <div className="flex bg-black items-start md:items-center min-h-fit lg:h-[92vh]">
+                <div className="flex flex-wrap justify-between items-start my-10 md:items-center w-full">
                     <ItemImageSet images={item.images} alt={item.name}/>
-                    <div className="md:flex-[45%] flex-[100%]">
-                        <h1 className="text-white font-medium">{item.name}</h1>
-                        <div className="flex items-center">
+                    <div className="flex-[100%] md:flex-[45%] ml-3 lg:ml-0">
+                        <h1 className="text-white font-medium mt-5 md:mt-0 leading-none">{item.name}</h1>
+                        <div className="flex items-center mt-5">
                             <h3 className="text-white font-thin tracking-wider mr-3">{item.rating}</h3>
-                            <RatingStars value={item.rating} readOnly />
+                            <RatingStars value={item.rating} />
                         </div>
-                        <p className="text-neutral-500 font-light">{`${item.ratingCount} Ratings | 53 Comments`}</p>
-                        <h2 className="text-carnation-500 tracking-wider">{`Rs ${item.price.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`}</h2>
+                        <p className="text-neutral-500 font-light mb-5">{`${item.ratingCount} Ratings | 53 Comments`}</p>
+                        <h2 className="text-carnation-500 tracking-wider mb-5">{`Rs ${item.price.toLocaleString('en-US', {minimumFractionDigits: 2, maximumFractionDigits: 2})}`}</h2>
                         <form onSubmit={handleSubmit}>
+                            <p className="text-neutral-500 font-normal mb-1">Quantity:</p>
                             <QuantityInput max={item.stock}/>
-                            <div className="flex items-center">
+                            <div className="mt-5">
+                                <span className="text-neutral-500 font-normal">Availability:</span>
+                                <span className="text-white font-normal ml-2">{item.stock}</span>
+                            </div>
+                            <div className="flex items-center mt-5">
                                 <Button name="action" value="buy" buttonLabel="Buy Now" variant="primary" buttonClassName="px-8 mr-6"></Button>
                                 <Button name="action" value="addToCart" buttonLabel="Add to Cart" variant="outline" buttonClassName="px-5"></Button>
                             </div>
