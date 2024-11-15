a5492318b12e903dee744d937dbfc3fc31312d8b
AshanSalinda <ashansalinda5@gmail.com>
2024-11-16 01:13:15 +0530

    Single Product Sections Updated

--- src/app/product/[productId]/page.tsx
+++ src/app/product/[productId]/page.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import item from "@/data/productIem.json";
-import ProductCard, { ProductCardProps } from "@/components/Product/ProductCard";
-import Overview from "@/components/Product/SingleProduct/Overview";
-import RatingAndReviews from "@/components/Product/SingleProduct/RatingAndReviews";
+import Overview from "@/section/productPageSections/Overview";
+import Description from "@/section/productPageSections/Description";
+import RatingAndReviews from "@/section/productPageSections/RatingAndReviews";
+import RelatedItems from "@/section/productPageSections/RelatedItems";
 
 const ProductItem : React.FC = () => {
     const commonData = {
@@ -35,45 +36,5 @@ const ProductItem : React.FC = () => {
     );
 };
 
-
-interface PropsType {
-    description: string,
-}
-
-const Description: React.FC<PropsType> = ({description}) => {
-    const isHTML = /<[a-z][\s\S]*>/i.test(description);
-
-    return (
-        <div className="px-3 py-7 md:p-7 box-border">
-            <h3 className="font-medium">Product Details</h3>
-            {isHTML ? (
-                <div className="m-3" dangerouslySetInnerHTML={{ __html: description }} />
-            ) : (
-                <div className="font-light m-3 text-justify">{description}</div>
-            )}
-        </div>
-    );
-};
-
-interface RelatedItemsType {
-    relatedItems: ProductCardProps[]
-}
-
-
-const RelatedItems: React.FC<RelatedItemsType> = ({ relatedItems }) => {
-    return (
-        <div className="px-3 py-7 md:p-7">
-            <h3 className="font-medium">Related Items</h3>
-
-            <div className="pt-5 pb-16 overflow-x-auto">
-                <div className="flex w-fit mx-auto space-x-3 ">
-                    {relatedItems.map((item, index) => (
-                        <ProductCard key={index} {...item} />
-                    ))}
-                </div>
-            </div>
-        </div>
-    );
-}
 
 export default ProductItem;
