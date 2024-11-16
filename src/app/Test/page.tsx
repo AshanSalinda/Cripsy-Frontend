1b8b2f4870cbf3bbaffa40ae4842950cb0de9a97
AshanSalinda <ashansalinda5@gmail.com>
2024-11-17 03:35:06 +0530

    Rich Text Editor

--- src/app/Test/page.tsx
+++ src/app/Test/page.tsx
@@ -1,11 +1,14 @@
-import BranchTable from '@/components/TestBranch/BranchTable'
 import React from 'react'
+import BranchTable from '@/components/TestBranch/BranchTable';
+import TextEditor from '@/components/TextEditor/TextEditor';
+
 
 const Page = () => {
   return (
     <div>
       <h2 className='text-4xl text-red-600' >Test Page</h2>
       <BranchTable />
+      <TextEditor className='w-4/6 mx-auto' />
     </div>
   )
 }
--- src/section/productPageSections/Overview.tsx
+++ src/section/productPageSections/Overview.tsx
@@ -1,11 +1,10 @@
 "use client";
-
 import React, { useState } from 'react';
 import ImageSet from "./ImageSet";
 import ProductDetails, { DetailsType } from './Details';
 import { IoMdHeart, IoMdHeartEmpty } from "react-icons/io";
 import { RiShareLine } from "react-icons/ri";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import Tooltip from '@/components/Tooltip/Tooltip';
 
 
 
@@ -36,22 +35,18 @@ const Overview: React.FC<PropsType> = (props) => {
     return (
         <div className="bg-black min-h-fit lg:h-[92vh]">
             <div className="flex justify-end items-center pt-5 px-2 text-carnation-500 text-4xl space-x-3 box-border">
-                <TooltipProvider><Tooltip>
-                    <TooltipTrigger onClick={handleFav} className='clickEffect'>
-                        { isFavorite ? <IoMdHeart/> : <IoMdHeartEmpty/> }
-                    </TooltipTrigger>
-                    <TooltipContent side="bottom">
-                        <p>{ isFavorite ? "Remove from Watchlist" : "Add to WatchList" }</p>
-                    </TooltipContent>
-                </Tooltip></TooltipProvider>
-                <TooltipProvider><Tooltip>
-                    <TooltipTrigger onClick={handleCopyUrl}>
-                        <RiShareLine className='clickEffect'/>
-                    </TooltipTrigger>
-                    <TooltipContent side="bottom">
-                        <p>Copy link</p>
-                    </TooltipContent>
-                </Tooltip></TooltipProvider>
+                <Tooltip 
+                    label={ isFavorite ? "Remove from Watchlist" : "Add to WatchList" }
+                    className='clickEffect'
+                    onClick={handleFav} >
+                    { isFavorite ? <IoMdHeart/> : <IoMdHeartEmpty/> }
+                </Tooltip>
+                <Tooltip 
+                    label='Copy link'
+                    className='clickEffect'
+                    onClick={handleCopyUrl} >
+                    <RiShareLine className='clickEffect'/>
+                </Tooltip>
             </div>
             <div className="flex bg-black items-start md:items-center min-h-fit h-full">
                 <div className="flex flex-wrap justify-between items-start mt-5 mb-10 md:items-center w-full">
