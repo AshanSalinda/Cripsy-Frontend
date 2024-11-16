97b2ca8af01a66e51316006627e7bf52e684c14e
Achala Imesh <imeshbaa.21@uom.lk>
2024-11-16 08:18:25 +0530

    Product Grid created

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import React from 'react';
-import { useRouter } from 'next/navigation';
+import React from "react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
-import CustomButton from '../Button/CustomButton';
+import CustomButton from "../Button/CustomButton";
 import { FaOpencart } from "react-icons/fa";
-import RatingStar from './RatingStar';
+import RatingStar from "./RatingStar";
 
 export interface ProductCardProps {
     productId?: string;
@@ -29,24 +29,26 @@ const ProductCard: React.FC<ProductCardProps> = ({
     const router = useRouter();
     const handleClick = () => {
         router.push(`/product/${productId}`);
-    }
+    };
 
     return (
-        <div onClick={handleClick} className="max-w-80 rounded-lg shadow-lg border border-gray-200 transform transition duration-300 hover:scale-[1.02] hover:shadow-2xl hover:border-carnation-200">
+        <div
+            onClick={handleClick}
+            className="max-w-80 rounded-lg shadow-lg border border-gray-200 transform transition duration-300 hover:scale-[1.02] hover:shadow-2xl hover:border-carnation-200"
+        >
             <div className="bg-gray-300">
                 <Image
-                    className="w-full h-72"
+                    className="w-full h-72 object-cover"
                     src={imageSrc}
                     width={300}
                     height={200}
-                    objectFit='cover'
                     priority
                     alt={title}
                 />
             </div>
             <div className="p-4">
-                <h3 className="text-xl font-semibold mb-1">{title}</h3>
-                <p className="text-gray-500 mb-2">{description}</p>
+                <h3 className="text-xl font-semibold mb-1 truncate">{title}</h3>
+                <p className="text-gray-500 mb-2 truncate">{description}</p>
                 <div className="flex items-center mb-4">
                     <RatingStar value={rating} readOnly={true} small={true} />
                     <span className="text-gray-600 ml-2">({reviews})</span>
@@ -58,9 +60,11 @@ const ProductCard: React.FC<ProductCardProps> = ({
                         buttonClassName="rounded-full px-4 py-2 transition font-semibold"
                         icon={<FaOpencart />}
                     />
-                    <div className="flex items-start space-x-1">
-                        <span className="text-sm align-text-top font-semibold">Rs</span>
-                        <span className="text-xl font-bold">{price.toFixed(2)}</span>
+                    <div className="flex items-baseline space-x-1 leading-none">
+                        <span className="text-sm font-semibold align-text-top">Rs</span>
+                        <span className="text-md font-bold whitespace-nowrap">
+                            {price.toFixed(2).toLocaleString()}
+                        </span>
                     </div>
                 </div>
             </div>
