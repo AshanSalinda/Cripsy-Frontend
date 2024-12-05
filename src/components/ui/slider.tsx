3b7a7aa718616395596e8fbc4572cc5e52963e09
Achala Imesh <imeshbaa.21@uom.lk>
2024-12-05 23:57:17 +0530

    fixed filter sidebar error

--- src/components/ui/slider.tsx
+++ src/components/ui/slider.tsx
@@ -4,14 +4,19 @@ import { cn } from "@/lib/utils";
 
 const Slider = React.forwardRef<
   React.ElementRef<typeof SliderPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, ...props }, ref) => (
+  React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root> & {
+    onValueChange?: (values: [number, number]) => void;
+    value: [number, number];
+  }
+>(({ className, value, onValueChange, ...props }, ref) => (
   <SliderPrimitive.Root
     ref={ref}
     className={cn(
       "relative flex w-full touch-none select-none items-center",
       className
     )}
+    value={value}
+    onValueChange={onValueChange}
     {...props}
   >
     <SliderPrimitive.Track className="relative h-2 w-full grow overflow-hidden rounded-full bg-gray-300">
@@ -19,10 +24,15 @@ const Slider = React.forwardRef<
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
       className="block h-5 w-5 bg-white border border-gray-400 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-carnation-500 focus:ring-offset-2 transition-transform transform active:scale-110"
-      aria-label="Slider Thumb"
+      aria-label="Slider Thumb 1"
+    />
+    <SliderPrimitive.Thumb
+      className="block h-5 w-5 bg-white border border-gray-400 rounded-full shadow-md focus:outline-none focus:ring-2 focus:ring-carnation-500 focus:ring-offset-2 transition-transform transform active:scale-110"
+      aria-label="Slider Thumb 2"
     />
   </SliderPrimitive.Root>
 ));
-Slider.displayName = SliderPrimitive.Root.displayName;
+
+Slider.displayName = "Slider";
 
 export { Slider };
