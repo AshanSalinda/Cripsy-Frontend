ac572af62b4b4eb3df88a3f8385678d736c31765
ISURU VITHANAGE <38922397+IsuruVithanage@users.noreply.github.com>
2024-12-07 10:41:46 +0530

    Customer Update

--- src/section/Customer/AwaitingOrderSection.tsx
+++ src/section/Customer/AwaitingOrderSection.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import {orderColumns, Order} from '@/components/Table/Columns';
 import TableWithPagi from '@/components/Table/TableWithPagi';
 import {getCustomerStatusedOrders} from "@/apis/orderApi/orderApi";
@@ -8,11 +8,10 @@ export const AwaitingOrderSection = () => {
     const [filteredData, setFilteredData] = useState<Order[]>([]);
 
     useEffect(() => {
-        const getData=async () => {
-            setFilteredData(await getCustomerStatusedOrders(1,"Placed"));
+        const getData = async () => {
+            setFilteredData(await getCustomerStatusedOrders(1, "Placed"));
         }
         getData()
-
     }, []);
 
     const handleEdit = (row: Order) => {
@@ -22,23 +21,22 @@ export const AwaitingOrderSection = () => {
     return (
         <>
             <div className="flex justify-between mb-3 mt-6">
-        <h5 className="flex items-center text-lg font-semibold font-inter">Refund Details</h5>
-    </div>
-    <div className="w-full">
+                <h5 className="flex items-center text-lg font-semibold font-inter">Refund Details</h5>
+            </div>
+            <div className="w-full">
 
-    <TableWithPagi<Order>
-        columns={orderColumns}
-    data={filteredData}
-    itemsPerPage={15}
-    className='custom-table-class '
-    getRowId={(row) => row.orderId}
-    handleEdit={handleEdit}
+                <TableWithPagi<Order>
+                    columns={orderColumns}
+                    data={filteredData}
+                    itemsPerPage={15}
+                    className='custom-table-class '
+                    getRowId={(row) => row.orderId}
+                    handleEdit={handleEdit}
 
+                />
 
-    />
+            </div>
 
-    </div>
-
-    </>
-)
+        </>
+    )
 }
